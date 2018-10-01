493736c3635a493ddc01ad9fe691c044cf461b4f
Wilson Barrios <wjjbarrios@gmail.com>
2018-10-01 10:56:00 -0600

    Se agrego el detalle

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listado),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detallepost, name='detallepost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
+
 
 def listado(request):
     posts = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar.html', {'posts': posts})
+
+def detallepost(request, pk):
+     posts = get_object_or_404(Publicacion, pk=pk)
+     return render(request, 'blog/detalle.html', {'posts': posts})
