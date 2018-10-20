56036c8f83a5ee77ae9e46ca239d5e79c03d27fe
Wilson Barrios <wjjbarrios@gmail.com>
2018-10-19 18:12:05 -0600

    se agrego como borrador la aplicacion y el boton eliminar

--- blog/views.py
+++ blog/views.py
@@ -13,18 +13,31 @@ def detallepost(request, pk):
      posts = get_object_or_404(Publicacion, pk=pk)
      return render(request, 'blog/detalle.html', {'posts': posts})
 
+def post_publish(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    post.publicar()
+    return redirect('detallepost', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    post.delete()
+    return redirect('listado')
+
 def post_new(request):
         if request.method == "POST":
             form = PostForm(request.POST)
             if form.is_valid():
                 post = form.save(commit=False)
                 post.autor = request.user
-                post.fecha_publicacion = timezone.now()
                 post.save()
                 return redirect('listado')
         else:
             form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    post = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/post_draft_list.html', {'post': post})
 
 def post_edit(request, pk):
         post = get_object_or_404(Publicacion, pk=pk)
